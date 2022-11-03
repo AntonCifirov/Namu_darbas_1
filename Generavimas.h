1e2e2b89f03cb6e98b853da20ff493d8f7037d2f
AntonCifirov <112755146+AntonCifirov@users.noreply.github.com>
2022-11-03 20:31:07 +0200

    Update Generavimas.h

--- Generavimas.h
+++ Generavimas.h
@@ -8,39 +8,6 @@ string generuoja_faila(int mokiniu_sk, int nd_sk);
 
 void RemoveStudent(vector<duom>& students, int gal);
 
-/*
-vector<duom> Failo_rusiavimas_vargseliai(vector<duom> asmuo_vector)
-{
- 
-    vector<duom> varg_vector;
-    for (vector<duom>::iterator it = asmuo_vector.begin(); it != asmuo_vector.end();)
-    {
-        cout << (*it).var << endl;
-        if ((*it).gal < 5)
-        {
-            varg_vector.push_back(*it);
-            cout <<"Deleting: " <<(*it).var << endl;
-            cout << "Deleting: " << (*it).pav << endl;
-            cout << "Deleting: " << (*it).gal << endl;
-
-            
-            asmuo_vector.erase(it);
-            cout<<(*it).pav << endl;
-            cout << "_____" << endl;
-            
-        }
-        else
-        {
-            it++;
-        }
-        //cout << iter << endl;
-        //cout << iter << endl;
-    
-    }
-
-    return varg_vector;
-
-}*/
 
 vector<duom> Failo_rusiavimas_vargseliai(vector<duom> asmuo_vector);
 
